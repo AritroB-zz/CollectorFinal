920cc3324b2d944c286bfae1698bd434082d8ee8
AritroB <abasuusa2010@gmail.com>
2020-08-14 18:50:30 -0500

    Added ViewControllers and connected all elements

--- Collector/CreateCollectableViewController.swift
+++ Collector/CreateCollectableViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class CreateCollectableViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func mediaFolderTapped(_ sender: Any) {
     }
-    */
-
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
 }
