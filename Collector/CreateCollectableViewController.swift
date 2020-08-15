618f5c55af5f24ca8871bb86ebe03ce67281546c
AritroB <abasuusa2010@gmail.com>
2020-08-14 19:16:42 -0500

    Added pickerController

--- Collector/CreateCollectableViewController.swift
+++ Collector/CreateCollectableViewController.swift
@@ -8,19 +8,29 @@
 
 import UIKit
 
-class CreateCollectableViewController: UIViewController {
+class CreateCollectableViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    var pickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        pickerController.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        <#code#>
     }
     
     @IBAction func mediaFolderTapped(_ sender: Any) {
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
     }
     @IBAction func cameraTapped(_ sender: Any) {
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
     }
     @IBAction func addTapped(_ sender: Any) {
     }
